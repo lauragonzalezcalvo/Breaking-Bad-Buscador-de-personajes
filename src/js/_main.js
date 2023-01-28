78c6d6c6fe52b80c10e871e82c2e29f5c7a4da62
lauragonzalezcalvo <laura24gc@gmail.com>
2023-01-28 18:24:24 +0100

    Add the characters.json

--- src/js/_main.js
+++ src/js/_main.js
@@ -1,41 +1,38 @@
-
-'use strict';
+"use strict";
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 /* eslint-disable camelcase */
 
-
 //---------------------------------------QUERYS SELECTOR ----------- LISTA CHARACTERS--------------------------------------
 
-const characterList = document.querySelector('.js-characterList'); 
-const btn = document.querySelector('.js-btn');
-const input = document.querySelector('.js_input');
-const favouritesList = document.querySelector('.js-favList');
-const resetBtn = document.querySelector('.js-resetBtn');
+const characterList = document.querySelector(".js-characterList");
+const btn = document.querySelector(".js-btn");
+const input = document.querySelector(".js_input");
+const favouritesList = document.querySelector(".js-favList");
+const resetBtn = document.querySelector(".js-resetBtn");
 
 // -----------------------------VARIABLES GLOBALES, la lista de personajes y la lista de personajes favoritos.--------------
-
 
 let allCharacters = [];
 let favouritesCharacters = [];
 
 //--------------------------------------------- FUNCIONES-------------------------------------------------------------------
 
-
 //Esta función me permite pasarle los parámetros, y al final tenemos que llamar a la función. la llamamos en la función renderCharacter
 // ponemos el id que será nuestro atributo gancho
 
-function renderOneCharacter (character){
-
+function renderOneCharacter(character) {
   // hacemos esta condicion para que aparezca seleccionada favourites
 
-  const characterInFavouritesIndex = favouritesCharacters.findIndex((eachCharacterObj) => eachCharacterObj.char_id === parseInt(character.char_id));
-  let classFavourite = '';
-  if (characterInFavouritesIndex === -1 ){
-    classFavourite ='';
-  }
-  else{
-    classFavourite = 'selected';
+  const characterInFavouritesIndex = favouritesCharacters.findIndex(
+    (eachCharacterObj) =>
+      eachCharacterObj.char_id === parseInt(character.char_id)
+  );
+  let classFavourite = "";
+  if (characterInFavouritesIndex === -1) {
+    classFavourite = "";
+  } else {
+    classFavourite = "selected";
   }
   return `
   <li class="js-character characters___Articles ${classFavourite}"id="${character.char_id}">
@@ -47,111 +44,109 @@ function renderOneCharacter (character){
   </li>`;
 }
 
-
 //Nos permite recorrer el array para pintarlo .
 
-function renderCharacters (allCharacters){ 
-  characterList.innerHTML = '';
-  for (let i= 0; i < allCharacters.length; i++) {
-    characterList.innerHTML += renderOneCharacter (allCharacters[i]);
+function renderCharacters(allCharacters) {
+  characterList.innerHTML = "";
+  for (let i = 0; i < allCharacters.length; i++) {
+    characterList.innerHTML += renderOneCharacter(allCharacters[i]);
   }
-  addCharacterListener ();
+  addCharacterListener();
 }
 
 // ------Esta función addCharacterListener, nos permite hacer click y crear un bucle para que pueda clicar en cada uno de ellos-
 //como es un array  no coge un elemento solo, coge varios, por eso  hay que hacer un bucle
 
-function addCharacterListener (){
-  const allCharactersArticles = document.querySelectorAll('.js-character');
+function addCharacterListener() {
+  const allCharactersArticles = document.querySelectorAll(".js-character");
   for (const character of allCharactersArticles) {
-    character.addEventListener('click', handleCharacters);
+    character.addEventListener("click", handleCharacters);
   }
 }
 
 //esta función la hemos llamado con el addEventListener para que nos permita marcar de momento como favoritos y añadir clase selected
 
-function handleCharacters (event){
-
-  event.currentTarget.classList.toggle('selected');
+function handleCharacters(event) {
+  event.currentTarget.classList.toggle("selected");
 
   //creamos una variable para utilizar el .find y que me traiga el objeto al que he dado click. Al cumplirse la condición.
 
-  const SelectedCharacters = allCharacters.find ((eachCharacterObj) => eachCharacterObj.char_id === parseInt(event.currentTarget.id));
+  const SelectedCharacters = allCharacters.find(
+    (eachCharacterObj) =>
+      eachCharacterObj.char_id === parseInt(event.currentTarget.id)
+  );
 
- //utilizamos finIndex. Condición: si existe ese id en la lista te la añade si ya está no te la añade
+  //utilizamos finIndex. Condición: si existe ese id en la lista te la añade si ya está no te la añade
 
-  const characterInFavouritesIndex = favouritesCharacters.findIndex((eachCharacterObj) => eachCharacterObj.char_id === parseInt(event.currentTarget.id));
+  const characterInFavouritesIndex = favouritesCharacters.findIndex(
+    (eachCharacterObj) =>
+      eachCharacterObj.char_id === parseInt(event.currentTarget.id)
+  );
 
-  if (characterInFavouritesIndex === -1 ){
+  if (characterInFavouritesIndex === -1) {
     favouritesCharacters.push(SelectedCharacters);
-    localStorage.setItem('characterFav', JSON.stringify(favouritesCharacters)); 
+    localStorage.setItem("characterFav", JSON.stringify(favouritesCharacters));
   }
   // Si volvemos a clickar nos lo quite de fav por eso utilizamos el findIndex en vez de find.
-
   else {
     favouritesCharacters.splice(characterInFavouritesIndex, 1);
-    localStorage.setItem('characterFav', JSON.stringify(favouritesCharacters)); 
+    localStorage.setItem("characterFav", JSON.stringify(favouritesCharacters));
   }
-  renderFavCharacters ();
+  renderFavCharacters();
 }
 
 //sirve para pintarlo en la lista de favoritos
 
-function renderFavCharacters (){
-  let html ='';
+function renderFavCharacters() {
+  let html = "";
   for (const character of favouritesCharacters) {
-    html += renderOneCharacter (character);
+    html += renderOneCharacter(character);
   }
   favouritesList.innerHTML = html;
 }
 
-  // recoger el valor de value y cuando le demos a search compararlo con el fetch y mostrarlo
-  //de cada uno de mis personajes me va a filtrar que su nombre incluya lo que hemos escrito.
+// recoger el valor de value y cuando le demos a search compararlo con el fetch y mostrarlo
+//de cada uno de mis personajes me va a filtrar que su nombre incluya lo que hemos escrito.
 
-function searchCharacters(event){
+function searchCharacters(event) {
   event.preventDefault();
   const inputValue = input.value.toLowerCase();
-  const filteredCharacters = allCharacters.filter((character) => character.name.toLowerCase().includes(inputValue));
+  const filteredCharacters = allCharacters.filter((character) =>
+    character.name.toLowerCase().includes(inputValue)
+  );
   renderCharacters(filteredCharacters);
 }
 
-function resetFav(ev){
+function resetFav(ev) {
   ev.preventDefault();
-  localStorage.removeItem('characterFav');
-  favouritesCharacters.length= [];
-  renderCharacters (allCharacters);
-  favouritesList.innerHTML ='';
-  resetBtn.classList.remove('hidden');
+  localStorage.removeItem("characterFav");
+  favouritesCharacters.length = [];
+  renderCharacters(allCharacters);
+  favouritesList.innerHTML = "";
+  resetBtn.classList.remove("hidden");
 }
-
 
 //--------------------------------------------------------- EVENTOS------------------------------------------------
 
-
-
-
-btn.addEventListener('click', searchCharacters);
-resetBtn.addEventListener('click', resetFav);
-
+btn.addEventListener("click", searchCharacters);
+resetBtn.addEventListener("click", resetFav);
 
 // -----------------------------------------CÓDIGO QUE SE EJECUTA AL CARGAR LA PÁGINA-------------------------------
 
-fetch('https://breakingbadapi.com/api/characters')
+fetch("./assets/data/characters.json")
   .then((response) => response.json())
   .then((data) => {
     allCharacters = data;
-    renderCharacters (allCharacters);
+    renderCharacters(allCharacters);
   });
-  
 
 // Pintar lo que está guardado en el localStorage
 
-const savedLocalFAvs = JSON.parse(localStorage.getItem('characterFav')); 
+const savedLocalFAvs = JSON.parse(localStorage.getItem("characterFav"));
 
 //puedo pasarlo como parametro o la llamo por una variable global
 
-if (savedLocalFAvs !== null){
+if (savedLocalFAvs !== null) {
   favouritesCharacters = savedLocalFAvs;
   renderFavCharacters();
 }
-
